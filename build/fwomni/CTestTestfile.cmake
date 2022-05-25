cc467eda2d4f829b1e545edb6dadc291d080270f
avdhootu27 <avdhootu.27@gmail.com>
2022-05-25 23:40:20 +0530

    Added basic urdf

---
+++ build/fwomni/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/avdhoot/fwomni_ws/src/fwomni
+# Build directory: /home/avdhoot/fwomni_ws/build/fwomni
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
