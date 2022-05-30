bc18ec635f8582315714bed11d49e42b6af72e81
avdhootu27 <avdhootu.27@gmail.com>
2022-05-30 18:06:22 +0530

    Noted information of needed topics

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/avdhoot/fwomni_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/avdhoot/swerve_ws/devel;/opt/ros/noetic'.split(';') if '/home/avdhoot/swerve_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/avdhoot/fwomni_ws/devel;/home/avdhoot/swerve_ws/devel;/opt/ros/noetic'.split(';') if '/home/avdhoot/fwomni_ws/devel;/home/avdhoot/swerve_ws/devel;/opt/ros/noetic' != '' else []
