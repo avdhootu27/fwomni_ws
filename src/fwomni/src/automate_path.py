e181ce0b9a84ec38ac2b266bf88dd2855510fbce
avdhootu27 <avdhootu.27@gmail.com>
2022-06-01 20:24:57 +0530

    Path automation done

--- src/fwomni/src/automate_path.py
+++ src/fwomni/src/automate_path.py
@@ -2,35 +2,38 @@
 
 import roslib
 import rospy
-from std_msgs import String
+from std_msgs.msg import String
+from std_msgs.msg import Header
+from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseActionResult
 
 path = [
-	[(1, 1, 0), (0,0,0,0)],
-	[(1, 2, 0), (0,0,0,0)],
-	[(1, 3, 0), (0,0,0,0)],
-	[(2, 3, 0), (0,0,0,0)],
+	[[1, 1, 0], [0,0,-0.00421336,0.9999911]],
+	[[1, 2, 0], [0,0,-0.00421336,0.9999911]],
+	[[1, 3, 0], [0,0,-0.00421336,0.9999911]],
+	[[2, 3, 0], [0,0,-0.00421336,0.9999911]],
 ]
 
-keepGoing = false
-currentGoalReached = false
-currentGoalHeader = Header()
+currentGoalStamp = 0
 
 def onCommand(msg):
 	if msg.data=="start":
-		keepGoing = true
+		startPath()
 	elif msg.data=="stop":
-		keepGoing = false
 		rospy.loginfo("Stopping path automation")
-		exit()
+		quit()
 
 def onGoalReached(msg):
+	global currentGoalStamp
 	if msg.status.text=="Goal reached.":
-		# mark goal as reached if timestamp of reached goal is equal to timestamp of current goal
-		if msg.status.goal_id.stamp==currentGoalHeader.stamp:
-			currentGoalReached = true
-			rospy.loginfo("Path in progress, goal "+str(currentGoalHeader.seq)+ "reached")
+		rospy.loginfo("Path in progress")
+		currentGoalStamp += 1
+		if currentGoalStamp>=len(path):
+			rospy.loginfo("Path complete")
+			quit()
+		else:
+			sendGoal(currentGoalStamp)
 
 def sendGoal(seq_):
 	poseStamp_ = PoseStamped()
@@ -41,43 +44,31 @@ def sendGoal(seq_):
 	header_.stamp = rospy.Time.now()
 	header_.frame_id = "map"
 
-	pose_.position = path[seq_][0]
-	pose_.orientation = path[seq_][1]
+	pose_.position.x = path[seq_][0][0]
+	pose_.position.y = path[seq_][0][1]
+	pose_.position.z = path[seq_][0][2]
+	pose_.orientation.x = path[seq_][1][0]
+	pose_.orientation.y = path[seq_][1][1]
+	pose_.orientation.z = path[seq_][1][2]
+	pose_.orientation.w = path[seq_][1][3]
 
 	poseStamp_.header = header_
 	poseStamp_.pose = pose_
 
-	currentGoalReached = false
-	currentGoalHeader = header_
 	to_move_base.publish(poseStamp_)
-	rospy.loginfo("Path in progress, reaching goal " + str(seq_))
+	rospy.loginfo("Path in progress")
 
 def startPath():
-	# wait till start command is not received
-	while(!keepGoing):
-		pass
-
-	l = len(path)
-	for i in range(l):
-		# send one goal
-		sendGoal(i)
-		# wait till current goal is not reached
-		while(!currentGoalReached):
-			# stop the path if stop command is received
-			if keepGoing==false:
-				return
-			pass
-	rospy.loginfo("Path complete")
-
+	rospy.loginfo("Start command received.")
+	currentGoalStamp = 0
+	sendGoal(0)
 
 if __name__ == '__main__':
-	# initialize the node
 	rospy.init_node('automate')
-
 	to_move_base = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
 	rospy.Subscriber("/move_base/result", MoveBaseActionResult, onGoalReached)
 	rospy.Subscriber("/automate/command", String, onCommand)
-	# start the path
-	startPath()
-	rospy.loginfo("Path automation stopped")
+	rospy.loginfo("Waiting for start command...")
 	rospy.spin()
+
+# rostopic pub /automate/command std_msgs/String "start"
